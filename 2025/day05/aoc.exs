d0dd0a802b2ae7f8a0829eb64041f88f0c5d6102
Érico Nogueira <erico.erc@gmail.com>
2025-12-06 11:14:25 -0300

    Update Advent 2025 - day 5.
    Implement part 2. Figuring out what the recursion would look like here
    with the changes to the list took a while! Using indexes ended up being
    the best way to do it, even if indexing lists isn't very efficient.

--- 2025/day05/aoc.exs
+++ 2025/day05/aoc.exs
@@ -2,6 +2,40 @@ defmodule AoC do
   def count_nums_in_range(nums, ranges) do
     Enum.count(nums, fn n -> Enum.any?(ranges, fn r -> n in r end) end)
   end
+
+  defp join_ranges(r1, r2) do
+    get_start_end = fn %Range{first: s, last: e} -> {s, e} end
+    {r1s, r1e} = get_start_end.(r1)
+    {r2s, r2e} = get_start_end.(r2)
+    min(r1s, r2s)..max(r1e, r2e)
+  end
+
+  defp scan_range(element, ranges) do
+    disjoint? = fn r -> Range.disjoint?(element, r) end
+    # joints will receive a range with element in it, which simplifies the logic for callers
+    {disjoints, joints} = Enum.split_with(ranges, disjoint?)
+
+    if Enum.empty?(joints) do
+      [element | disjoints]
+    else
+      scan_range(Enum.reduce(joints, element, &join_ranges/2), disjoints)
+    end
+  end
+
+  defp accumulate_ranges(ranges, idx) do
+    ranges = scan_range(Enum.at(ranges, idx), ranges)
+
+    if idx + 1 >= Enum.count(ranges) do
+      ranges
+    else
+      accumulate_ranges(ranges, idx + 1)
+    end
+  end
+
+  def total_elements_ranges(ranges) do
+    ranges = accumulate_ranges(ranges, 0)
+    Enum.sum(Enum.map(ranges, &Enum.count/1))
+  end
 end
 
 [file_name] = System.argv()
@@ -17,3 +51,4 @@ fresh_ranges = for [b, e] <- fresh_ranges, do: b..e
 ingredients = for n <- String.split(hd(tl(parts))), do: String.to_integer(n)
 
 IO.inspect(AoC.count_nums_in_range(ingredients, fresh_ranges))
+IO.inspect(AoC.total_elements_ranges(fresh_ranges))
